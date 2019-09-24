a7004f4571785f36a12f38885e43dcd8fba8da47
Nikos Ntemkas <nntemkas@ots.gr>
2019-09-24 11:16:17 +0300

    sdsa

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>Server Details are currently not updated</p>
+        <p v-if='!server'>NO SERVER SELECTED</p>
+        <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+        <button @click="setNormal">Return to Normal</button>
+    </div>
+
+</template>
+
+<script>
+    import {serverBus} from '../../main.js';
+    export default{
+        data: function() {
+            return{
+                server: null
+            }
+        },
+        methods:{
+            setNormal(){
+                this.server.status='Normal'
+                
+            }
+        },
+        created(){
+            serverBus.$on('serverSelect',(server) => {
+                            this.server = server;
+                        })
+        }
+}
+</script>
+
+<style>
+
+</style>
